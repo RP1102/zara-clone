0290f91171a87295923659afa96e0d1ca4fcbd89
Romain Pontacq <romain.pontacqpro@gmail.com>
2022-09-17 20:19:52 +0200

    fixed elements pretty much done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import Wrapper from "./components/Wrapper/Wrapper";
 import "./App.scss";
+import logo from "./assets/logo.svg"
 
 function App() {
   return (
     <div className="App">
 
-      {/* <div className="fixed-stuff">
-        <h1>ZARA</h1>
-      </div> */}
+      <div className="fixed-stuff">
+        <div className="header">
+          <div className="header-left">
+            <div className="menu-btn">
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+            </div>
+            <img className="logo-zara" src={logo} alt="" />
+          </div>
+          <div className="header-right">
+            <p className="hr-elem search-bar">RECHERCHER</p>
+            <p className="hr-elem">SE CONNECTER</p>
+            <p className="hr-elem">AIDE</p>
+          </div>
+        </div>
+      </div>
 
       <Wrapper></Wrapper>
     </div>
